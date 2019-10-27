ce754c13842b13a3fa8cfca553bbec36ae3204b8
raulserrano <raulangel.serrano@gmail.com>
2019-10-27 19:20:29 +0100

    raul

---
+++ unidad 2/operadores_aritmeticos.js
@@ -0,0 +1,15 @@
+/*
+    Operadores artiméticos
+    +   -   *   /   %   ++   --
+*/
+
+let num1 = 12;
+let num2 = 5;
+
+console.log(`Suma: ${ num1 + num2 }`);
+console.log(`Resta: ${ num1 - num2 }`);
+console.log(`Multiplicación: ${ num1 * num2 }`);
+console.log(`División: ${ num1 / num2 }`);
+console.log(`Módulo: ${ num1 % num2 }`);
+console.log(`Incremento: ${ ++num1 }`); //Incrementa antes de la sentencia
+console.log(`Incremento: ${ num1-- }`); //Decrementa después de la sentencia.
---
+++ unidad 2/operadores_comparacion.js
@@ -0,0 +1,21 @@
+/*
+    A nivel de operadores, javascript comparte sintaxis con 
+    lenguajes como c, java, php..
+
+    Operadores de comparación, devuelven un valor booleano
+    ==  !=  === !==  >  >=  <  <=
+*/
+
+let v1 = 5;
+let v2 = 7;
+
+console.log(v1 > v2); //false
+console.log(v1 < v2); //true
+console.log(v1 != v2); //true
+
+v2 = '5';
+console.log(v1 == v2); // true: No comprueba tipos de datos.
+console.log(v1 === v2); //false
+
+
+
