ee21049c87dc0396de9db9a2d6166b3385513522
raulserrano <raulangel.serrano@gmail.com>
2019-10-27 18:29:56 +0100

    raul

---
+++ unidad 2/conversion_tipos.js
@@ -0,0 +1,17 @@
+/* 
+    Podemos cambiar los tipos de datos con parseInt, y toString
+*/
+let variable='5';
+console.log(`${variable} tipo: ${typeof(variable)}`)
+
+let variable2 = parseInt(variable);   //Es un método de window
+console.log(`${variable2} tipo: ${typeof(variable2)}`)
+
+let variable3 = variable2.toString(); //Es un método del objeto
+console.log(`${variable3} tipo: ${typeof(variable3)}`)
+
+// El único tipo de datos numerico es number, aún así podemos 
+// usar parseFloat para intercambiar entre reales y enteros.
+
+var num=3.14
+console.log('Tipo: '+typeof(num)+' '+parseInt(num))
--- unidad 2/tipos_de_datos.js
+++ unidad 2/tipos_de_datos.js
@@ -3,7 +3,7 @@
   dependerá del tipo de datos que asignemos un puede cambiar durante la ejecución
 */
 let variable = 7;
-console.log('Valor:' + variable +'typeof: ' +typeof(variable));
+console.log('Valor:' + variable +'typeof: ' + typeof(variable));
 
 variable='raul';
 console.log(`Valor: ${variable} typeof: ${typeof(variable)}`);//ES6 otra forma de mostrar cadenas
@@ -11,8 +11,8 @@ console.log(`Valor: ${variable} typeof: ${typeof(variable)}`);//ES6 otra forma d
 variable=true;
 console.log(`Valor: ${variable} typeof: ${typeof(variable)}`);
 
-variable=5
-variable2='hola'
+variable=5;
+variable2='hola';
 total= variable + variable2;
 //Conversión automática de tipos en una operación
 console.log(`Valor: ${total} typeof: ${typeof(total)}`);
