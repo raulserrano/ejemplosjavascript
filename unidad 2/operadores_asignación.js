4445fb4f66a9dd10fdc6f5e260a52c75dda4ba6f
raulserrano <raulangel.serrano@gmail.com>
2019-10-28 09:44:52 +0100

    raul

---
+++ unidad 2/operadores_asignación.js
@@ -0,0 +1,17 @@
+/*
+   Operador de asignación: =    
+   Realizan la operación y después asignan +=    -=    *=    /=    %=   
+   Realizan la operación (bits,poco utilizadas) <<=    >>= y después asignan
+*/
+
+let v1 = 2;
+console.log(v1); 
+
+v1 += 2;
+console.log(v1)
+
+v1 *= 2;
+console.log(v1);
+
+v1 <<= 1;// Operación binaria 1000 -> 10000
+console.log(v1);
