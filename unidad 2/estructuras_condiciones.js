02332a1800195b09d102311cf0eb6674e3ed6789
raulserrano <raulangel.serrano@gmail.com>
2019-10-28 18:20:16 +0100

    Raul

---
+++ unidad 2/estructuras_condiciones.js
@@ -0,0 +1,36 @@
+/*
+    if Permite la ejecución de código en dependiendo de una condición
+    puede ir acom panado de else o no.
+    Podemos usar switch cuando evaluemos varios valores.
+*/
+
+if ( 10 > 5) console.log('La condición es cierta');
+
+if ( 10 < 5) console.log('La condición es cierta');
+else console.log('La condición es falsa');
+
+//Si ejecutamos más de una instrucción las ponemos en un bloque de contenido {}
+let edad=20;
+if ( edad >= 18 ){
+    console.log('eres mayor de edad');
+    console.log('ya puedes conducir');
+} else {
+    console.log('eres menor de edad');
+    console.log('no puedes conducir');
+}
+
+let letra='A';
+switch (true) {
+    case 'A': console.log('switch dice: la letra es A')
+        break;
+    case 'B': console.log('switch dice: la letra es B')
+        break;
+    case 'C': console.log('switch dice: la letra es B')
+        break;
+    default: 
+             console.log('switch dice: la letra no es ni A, ni B, ni C')
+    break;
+       
+}
+
+
