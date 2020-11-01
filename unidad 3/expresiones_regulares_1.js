5e038177e41b3eaee5c38a6fc068c0317a487941
raulserrano <raulangel.serrano@gmail.com>
2020-11-01 12:09:11 +0100

    Terminado expresiones regulares

---
+++ unidad 3/expresiones_regulares_1.js
@@ -0,0 +1,37 @@
+/* 
+Expresiones regulares
+----------------------
+
+Son patrones que describen la como tiene que ser una cadena.
+
+Ejemplo: "Que la cadena empiece por a"
+
+Dos formas de definirlos:
+
+let patron = new RegExp('expresión_regular','modificadores') //mod. opcionales (igm)
+let patron = /expresión_regular/modificadores
+
+*/
+
+
+//El objeto RegExp dispone de un método test para comprobar si una cadena cumple el patrón
+
+let patron1 = /hola/;
+let patron2 = /a/g; //modificador g: Devuelve todas las coincidencias del patrón
+let cadena = 'amapola';
+
+console.log(patron1.test(cadena)); //false
+console.log(patron2.test(cadena)); //true
+
+//El objeto String también tiene métodos relacionados con la expresiones regulares
+
+console.log( cadena.match(patron1) ); //null
+console.log( cadena.match(patron2) ); // ['a','a','a']
+
+console.log(cadena.replace(/a/g,'X')); //XmXpolX (Xmapola sin g)
+console.log(cadena) //amapola (no modifica la cadena)
+
+
+
+
+
---
+++ unidad 3/expresiones_regulares_2.js
@@ -0,0 +1,87 @@
+/* 
+    CONTRUCCIÓN EXPRESIONES REGULARES
+*/
+
+    let cadena = 'Desarrollo de aplicaciones web 2020';
+
+/* 
+    CLASES DE CARACTERES 
+
+    \d Dígito
+    \s Espacio
+    \w Caracter (dígito, letra, guión bajo)
+    .  Cualquier caracter
+
+    Nota: Si ponemos la letra en mayúscula es la clase inversa
+
+    */
+
+    let patron = /\d/g;
+
+    console.log(patron.test(cadena)) //true
+    console.log(cadena.match(patron)) //['2','0','2','0']
+
+    patron = /de\s/; //Podemos combinar caracteres normales on clases de caracteres.
+
+    console.log(patron.test(cadena)) //true
+
+    /* 
+        INICIO  ^  Y FIN $ DE CADENA
+    */
+    
+    patron = /^D/;
+    console.log(patron.test(cadena)); //true
+
+    patron = /0$/;
+    console.log(patron.test(cadena)); //true
+
+    patron = /^D0$/;
+    console.log(patron.test(cadena)); //false ¿Reflexiona por que es falso
+
+    /* 
+        Conjuntos [...] : Cualquier caracter delos que indica.
+    */
+
+    patron = /[DXP]/
+    console.log(patron.test(cadena)); //true : Suficiente con que coincida D
+
+    patron = /[0-5]/
+    console.log(patron.test(cadena)); //true : El guión nos permite declarar rangos
+
+    patron = /^[0-5]/
+    console.log(patron.test(cadena)); //false : ^ Niega el elemento.
+
+    /*
+    CUANTIFICADORES : Se aplican al elemento que les precede
+
+    {n} : n veces (también admite rangos)
+    
+    */
+
+    patron = /20{2}/;
+    console.log(patron.test(cadena)); //false. Reflexiona por qué
+
+
+    patron = /(20){2}/;
+    console.log(patron.test(cadena)); //true. Los parentesis permiten agrupar
+
+    patron = /(20){3,5}/;
+    console.log(patron.test(cadena)); //false. Los rangos se ponen con comas
+
+    /*  Cuantificadores breviados
+
+    + : 1 o más {1,}
+    * : 0 o más {0,}
+    ? : 0 o 1   {0,1}
+
+    */
+
+    //nota, si en alguna expresión regular queremos burcar alguno de los caracteres
+    //especiales utilizados para crearlas, utilizaremos el operador de escapo \
+
+    // Ejercicio propuesto. Describe un patrón para comprobar si una dirección
+    // IP es correcta
+
+
+
+
