3cb0dcb6b330d3fef7d220944b3c935ccfb7b1dd
raulserrano <raulangel.serrano@gmail.com>
2022-10-20 13:29:07 +0200

    modificado:     unidad 3/String.js

--- unidad 3/String.js
+++ unidad 3/String.js
@@ -16,29 +16,42 @@
     */
 
     let num = 3;
-    cad = `CADENA${num}`; console.log(cad); //CADENA3
+    cad = `CADENA${num}`; console.log(cad); //->CADENA3
 
     //Tambien podemos usar caracteres especiales
     console.log('primera linea \nsegunda linea');
 
     //Propiedad length
 
-    console.log(cad.length);
+    console.log(cad.length); //->7
 
     // Métodos: Disponemos de diferentes métodos para trabajar con cadenas, siempre generando otra
     // ya que las cadenas son inmutables. 
 
     // charAt, toUpperCase, toLowerCase, indexOf, includes, substring, substr 
 
-    
+    cad = 'Cifp Carlos III';
+
+    console.log(cad.charAt(1));//->i
+
+    console.log(cad.toUpperCase());//->CIFP CARLOS III
+
+    console.log(cad.toLowerCase());//->cifp carlos iii
+
+    console.log(cad.indexOf('III'));//->12
+
+    console.log(cad.includes('III'));//->true
+
+    console.log(cad.substring(2,4));//->fp
 
 
     /*     
-    
             Tratamiento como array: cuando vamos la unidad de los arrays veremos que tambien
            se pueden tratar las cadenas como un array con un caracter en cada una de sus posiciones.
     
     */
+
+    console.log(cad[0]);//->C
 
 
 
