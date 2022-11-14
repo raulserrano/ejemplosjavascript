297799a3f656f01e4a9b66c8db43ac9b51ca3406
raulserrano <raulangel.serrano@gmail.com>
2022-11-14 17:27:46 +0100

    modificado:     unidad 3/01_window.htm 	modificado:     unidad 3/Math.js 	modificado:     unidad 4/01_arrays.htm 	modificado:     unidad 4/funciones.htm

--- unidad 3/Math.js
+++ unidad 3/Math.js
@@ -12,8 +12,8 @@ console.log("Math.SQRT2: "+Math.SQRT2);
 
 
 
-console.log("Math.ceil(3.14): "+ Math.ceil(3.14));
-console.log("Math.floor(3.14) " + Math.floor(3.14));
+console.log("Math.ceil(3.14): "+ Math.ceil(3.14));//->4
+console.log("Math.floor(3.14) " + Math.floor(3.14));//->3
 console.log("Math.pow(2,3) " + Math.pow(2,3));
 console.log("Math.random() " + Math.random());
 console.log("Math.min(5,7,8,3) " + Math.min(5,7,8,3));
