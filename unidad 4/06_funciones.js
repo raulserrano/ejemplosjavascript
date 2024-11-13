94b240d09a79fcaea0cf6e489d7a55091fc06d77
John Doe <raulangel.serrano@gmail.com>
2024-11-13 18:12:39 +0100

    Actualización módulos

---
+++ unidad 4/06_funciones.js
@@ -0,0 +1,15 @@
+// Declaración de funciones de la aplicación.
+
+export function sumar(a, b) {
+    return a + b;
+}
+
+export function restar(a, b) {
+    return a - b;
+}
+
+export class prueba {
+    constructor(nombre) {
+        this.nombre=nombre
+    }
+}
---
+++ unidad 4/06_script.js
@@ -0,0 +1,14 @@
+import { sumar } from './06_funciones.js';
+import { restar } from './06_funciones.js';
+import { prueba } from './06_funciones.js';
+
+console.log(typeof prueba);
+
+
+
+console.log((sumar(20, 10))); //
+console.log((restar(20, 10))); // 
+
+let num = new prueba('raul')
+console.log(num);
+
